b80e85a5c202b18b6158c753c05fc273a3b9af0b
jayoon16 <ooncheesenc1.17@ichat.sp.edu.sg>
2019-12-03 12:30:24 +0800

    Implement navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,152 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+  <v-app id="inspire">
+    <navbar v-if="!$route.meta.hideNavigation"></navbar>
 
     <v-content>
-      <HelloWorld />
+      <v-container fluid>
+        <v-row align="center" justify="center">
+          <v-col class="text-center">
+            <router-view></router-view>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-content>
+    <v-footer v-if="!$route.meta.hideNavigation" color="#1867C0" app>
+      <span class="white--text">&copy; 2019</span>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Navbar from './components/Navbar.vue';
 
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
+  props: {
+    source: String,
   },
 
-  data: () => ({
-    //
-  }),
+  components: {
+    Navbar,
+  },
+  data() {
+    return {};
+  },
 };
 </script>
+
+<style lang="scss">
+@font-face {
+  font-family: "Monst Regular";
+  src: url("fonts/Montserrat-Regular.otf");
+}
+@font-face {
+  font-family: "Monst SemiBold";
+  src: url("fonts/Montserrat-SemiBold.otf");
+}
+a {
+  text-decoration: none;
+}
+.container {
+  padding-top: 0;
+}
+.v-application .text-center {
+  padding-top: 0;
+}
+.page-header {
+  padding: 10px 0;
+  margin-bottom: 40px;
+  border-bottom: 1px #c2c2c2 solid;
+  text-align: left;
+  a {
+    visibility: hidden;
+  }
+  span {
+    font-family: "Monst SemiBold";
+    padding-left: 20px;
+    font-size: 18px;
+  }
+}
+.page-header-nav {
+  padding: 10px 0;
+  margin-bottom: 40px;
+  border-bottom: 1px #c2c2c2 solid;
+  text-align: left;
+  i {
+    font-size: 24px !important;
+    margin-right: 0 !important;
+  }
+  .text {
+    font-family: "Monst SemiBold";
+    padding-left: 20px;
+    font-size: 18px;
+  }
+}
+.form-right {
+  text-align: right !important;
+  max-width: 500px;
+  width: 100%;
+  margin: 0;
+}
+
+#signin {
+  .v-application--wrap {
+    min-height: 97vh;
+  }
+  .v-content {
+    height: 100%;
+  }
+}
+
+.venue-table {
+  td {
+    padding: 0 !important;
+    &:first-child {
+      padding: 0 16px !important;
+    }
+  }
+  .v-toolbar--flat {
+    height: 10px !important;
+  }
+  .v-data-table {
+    tbody {
+      tr.v-data-table__expanded__content {
+        box-shadow: none !important;
+        background-color: #e9e9e9;
+      }
+    }
+  }
+  .v-data-table-header-mobile__wrapper {
+    visibility: hidden;
+  }
+  .v-data-table-header-mobile__wrapper .v-select {
+    margin-bottom: 0px;
+  }
+  .v-data-table__mobile-row {
+    display: table-cell;
+  }
+  .v-data-table .v-data-table__mobile-row {
+    height: 48px;
+  }
+  .v-data-table__mobile-row:first-child .v-data-table__mobile-row__cell {
+    text-align: left;
+  }
+  .v-data-table__mobile-row .v-data-table__mobile-row__cell {
+    text-align: start;
+  }
+  .v-data-table__mobile-row:nth-child(2) {
+    width: 45px;
+  }
+  .v-data-table__mobile-row:nth-child(3) {
+    width: 32px;
+  }
+  .v-data-table__wrapper tr .v-data-table__mobile-row {
+    border-bottom: 1px solid rgba(0, 0, 0, 0.12);
+  }
+  .v-data-table__wrapper tr:first-child .v-data-table__mobile-row {
+    border-top: 1px solid rgba(0, 0, 0, 0.12);
+  }
+  .v-data-table-header-mobile th {
+    border-bottom: none !important;
+  }
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list dense>
+        <v-list-item link to="/Venue">
+          <v-list-item-content>
+            <v-list-item-title>Venue and Fields</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-content>
+            <v-list-item-title>Promotion Code</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-content>
+            <v-list-item-title>Announcement</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-content>
+            <v-list-item-title>Customers</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-content>
+            <v-list-item-title>Timing Discounts</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link to="/AdminAccounts">
+          <v-list-item-content>
+            <v-list-item-title>Admin Accounts</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="#1867C0" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>The Cage Admin Portal</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <div class="my-2">
+        <v-btn text large @click="logout()">Sign Out</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn text><v-icon>mdi-settings</v-icon></v-btn>
+      </div>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data: () => ({
+    drawer: null,
+  }),
+  methods: {
+    logout() {
+      localStorage.removeItem('user-token');
+      this.$router.go('');
+    },
+  },
+};
+</script>
+
+<style></style>
