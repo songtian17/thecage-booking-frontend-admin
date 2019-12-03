05782ce803c3d56ec5e11369707b1515cb40b6ce
jayoon16 <ooncheesenc1.17@ichat.sp.edu.sg>
2019-12-03 12:29:35 +0800

    Add confirm and error modals

---
+++ src/components/ConfirmationModal.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="showDialog" persistent max-width="350">
+      <v-card>
+        <v-card-title class="headline">Alert</v-card-title>
+
+        <v-card-text>
+          Are you sure you want to perform this action?
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn color="green darken-1" text @click="clickedCancel">Cancel</v-btn>
+          <v-btn color="green darken-1" text @click="clickedConfirm">OK</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: {
+    showDialog: {
+      default: () => false,
+      type: Boolean,
+    },
+  },
+  methods: {
+    clickedConfirm() {
+      this.$emit('confirm');
+    },
+    clickedCancel() {
+      this.$emit('cancel');
+    },
+  },
+};
+</script>
---
+++ src/components/ErrorModal.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="showDialog" persistent max-width="350">
+      <v-card>
+        <v-card-title class="headline" style="padding: 30px 24px 30px" text-center>
+          <v-icon class="mr-2" style="font-size:70px;width:100%;" color="red">mdi-alert</v-icon>
+        </v-card-title>
+
+        <v-card-text style="padding: 0 24px 0px;text-align:center;">{{ msg }}</v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn color="green darken-1" text @click="close">OK</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: {
+    showDialog: {
+      default: () => false,
+      type: Boolean,
+    },
+    msg: {
+      default: () => '',
+      type: String,
+    },
+  },
+  methods: {
+    close() {
+      this.$emit('close');
+    },
+  },
+};
+</script>
