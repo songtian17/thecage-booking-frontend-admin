65b975bcf2a0654d20eb09b5c4c520ac787f366a
jayoon16 <ooncheesenc1.17@ichat.sp.edu.sg>
2019-12-03 12:39:17 +0800

    Add components to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,9 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Signin from '../views/SignIn.vue';
+import Venue from '../views/Venue.vue';
+import AddVenue from '../views/AddVenue.vue';
+import EditVenue from '../views/EditVenue.vue';
 import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
@@ -7,10 +11,34 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home,
+    name: 'signin',
+    component: Signin,
+    meta: { hideNavigation: true, isSignedIn: true },
   },
   {
+    path: '/Venue',
+    name: 'Venue',
+    component: Venue,
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: '/AddVenue',
+    name: 'AddVenue',
+    component: AddVenue,
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: '/EditVenue/:id',
+    name: 'EditVenue',
+    component: EditVenue,
+    meta: {
+      requiresAuth: true,
+    },
+  },
     path: '/about',
     name: 'about',
     // route level code-splitting
@@ -24,6 +52,34 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (localStorage.getItem('user-token') == null) {
+      next({
+        path: '/',
+        params: { nextUrl: to.fullPath },
+      });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+
+  if (to.matched.some(record => record.meta.isSignedIn)) {
+    if (localStorage.getItem('user-token') != null) {
+      next({
+        path: '/Venue',
+        params: { nextUrl: to.fullPath },
+      });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
