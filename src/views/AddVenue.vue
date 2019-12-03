631439bfc45ac494298f54e105b29bb449bb066e
jayoon16 <ooncheesenc1.17@ichat.sp.edu.sg>
2019-12-03 12:37:04 +0800

    Implement views for add and edit venue

---
+++ src/views/AddVenue.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="page-header-nav">
+      <v-btn icon to="/Venue">
+        <v-icon small class="mr-2" style="margin-right:0!important">mdi-chevron-left</v-icon>
+      </v-btn>
+      <span class="text">Add Venue</span>
+    </div>
+
+    <v-form ref="form" v-model="isFormValid" class="addvenue" lazy-validation>
+      <v-container class="form-right">
+        <v-text-field
+          v-model="venuename"
+          :rules="nameRules"
+          label="Venue Name"
+          required
+        ></v-text-field>
+        <v-btn color="primary" class="mr-4" @click="submit">Add </v-btn>
+      </v-container>
+    </v-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    isFormValid: false,
+    venuename: '',
+    nameRules: [v => !!v || 'Venue Name is required'],
+  }),
+  methods: {
+    submit() {
+      if (this.$refs.form.validate()) {
+        // TODO: submit form to back end
+      }
+    },
+  },
+};
+</script>
---
+++ src/views/EditVenue.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="page-header-nav">
+      <v-btn icon to="/Venue">
+        <v-icon small class="mr-2" style="margin-right:0!important">mdi-chevron-left</v-icon>
+      </v-btn>
+      <span class="text">Edit Venue</span>
+    </div>
+
+    <v-form ref="form" v-model="isFormValid" class="addvenue" lazy-validation>
+      <v-container class="form-right">
+        <v-text-field
+          v-model="venueName"
+          :rules="nameRules"
+          label="Venue Name"
+          required
+        ></v-text-field>
+        <v-btn class="mr-4" color="primary" @click="submit">save changes</v-btn>
+      </v-container>
+    </v-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isFormValid: false,
+      venueName: '',
+      venueId: this.$route.params.id,
+      nameRules: [v => !!v || 'Venue Name is required'],
+    };
+  },
+  methods: {
+    submit() {
+      if (this.$refs.form.validate()) {
+        // TODO: submit form to back end
+      }
+    },
+  },
+};
+</script>
